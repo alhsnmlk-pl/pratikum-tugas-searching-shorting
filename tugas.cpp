9de27a70732bf2d9d35823a3ba9ce8bb9851bd55
alhsnmlk-pl <aallhsnmlk@gmail.com>
2026-06-07 22:23:19 +0700

    Refactor HP sorting and searching logic in tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -1,72 +1,52 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-int main(){
-    int L[12] = {0, 8, 1, 2, 5, 2, 8, 7, 8, 3, 5, 0};
-    int n = 12;
+int main (){
+    int array[12] = {0, 8, 1, 2, 5, 2, 8, 7, 8, 3, 5, 0};
     int i, k, temp;
-    int target;
-    int A, Z, K;
-    int ketemu = 0;
-
-    cout << "Data nomor HP sebelum diurutkan : ";
-    for(i = 0; i < n; i++){
-        cout << setw(4) << L[i];
+    int target, A, K, Z;
+    
+    cout << "Nomor HP (Belum diurutkan) :";
+    for(i=0; i<=11; i++){
+        cout << array[i] << " ";
     }
-
-    cout << endl;
-
-    for(i = 0; i < n; i++){
-        for(k = n - 1; k > i; k--){
-            if(L[k] < L[k - 1]){
-                temp = L[k];
-                L[k] = L[k - 1];
-                L[k - 1] = temp;
+    cout << "\n";
+    
+    for(i=0; i<=11; i++){
+        for(k=11; k>=1; k--){
+            if (array[k] < array[k-1]){
+                temp = array[k];
+                array[k] = array[k-1];
+                array[k-1] = temp;
             }
         }
     }
-
-    cout << "Data nomor HP setelah diurutkan : ";
-    for(i = 0; i < n; i++){
-        cout << setw(4) << L[i];
+    
+    cout << "Hasil urut ascending :";
+    for(i=0; i<=11; i++){
+        cout << array[i] << " ";
     }
-
-    cout << endl;
-    cout << "Masukkan angka yang dicari : ";
+    cout << "\n";
+    
+    cout << "Masukkan nilai angka yang dicari : ";
     cin >> target;
-
+    
     A = 0;
-    Z = n - 1;
-
+    Z = 11;
     while(A <= Z){
-        K = (A + Z) / 2;
-
-        if(target == L[K]){
-            ketemu = 1;
-            break;
-        }
-        else if(target < L[K]){
-            Z = K - 1;
-        }
-        else{
-            A = K + 1;
-        }
+        K = (A + Z)/2;
+        if(target > array[K])
+            { A = K + 1; }
+        else if(target < array[K])
+            { Z = K - 1; }
+        else
+            { A = Z + 1; }
     }
-
-    if(ketemu == 1){
-        cout << "Angka " << target << " ditemukan" << endl;
-        cout << "Angka berada di index ke : ";
-
-        for(i = 0; i < n; i++){
-            if(L[i] == target){
-                cout << i << " ";
-            }
-        }
-    }
-    else{
-        cout << "Data tidak ditemukan.";
-    }
-
+    
+    if(target == array[K])
+        { cout << "Data ditemukan di index ke : " << K << endl; }
+    else
+        { cout << "Data tidak ditemukan." << endl; }
+        
     return 0;
 }
